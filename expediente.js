626a0b0af0c825e77f74ba2a594571fe578f2ac4
Lariosk <105257073+Lariosk@users.noreply.github.com>
2022-09-22 09:55:11 -0500

    Create expediente.js
    Se programa expediente del paciente

---
+++ expediente.js
@@ -0,0 +1,11 @@
+{
+    "workbench.startupEditor": "none",
+    "editor.minimap.enabled": false,
+    "workbench.statusBar.visible": false,
+    "workbench.colorTheme": "Quiet Light",
+    "debug.disassemblyView.showSourceCode": false,
+    "workbench.editor.enablePreview": false,
+    "files.autoSave": "afterDelay",
+    "terminal.integrated.defaultProfile.windows": "Git Bash",
+    "php.validate.executablePath": ""
+}
